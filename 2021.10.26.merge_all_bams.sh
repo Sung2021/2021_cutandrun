5efc22998286d5d3390906d2f7bae3437002dea8
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2021-11-09 10:50:13 -0500

    Update 2021.10.26.merge_all_bams.sh

--- 2021.10.26.merge_all_bams.sh
+++ 2021.10.26.merge_all_bams.sh
@@ -4,10 +4,10 @@
 path=~/Desktop/Sung_work/raw_tmp/TLE3_cut_run
 cd ~/Desktop/Sung_work/raw_tmp/TLE3_cut_run/sorted_bam
 
-# samtools merge -@ 10 -f -o $path/all.merged.bam *bam 
+samtools merge -@ 10 -f -o $path/all.merged.bam *bam 
 
-# mkdir $path/macs2_all
-# macs2 callpeak -t $path/all.merged.bam -f BAMPE -g mm -n union -B -q 0.05 --SPMR --outdir $path/macs2_all
+mkdir $path/macs2_all
+macs2 callpeak -t $path/all.merged.bam -f BAMPE -g mm -n union -B -q 0.05 --SPMR --outdir $path/macs2_all
 
 #mkdir $path/macs2_merged_control
 #for input in *bam; 
