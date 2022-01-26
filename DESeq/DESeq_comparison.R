f1bfe7e6c5f5280a3058d08ad937d57569c8596d
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-01-26 10:03:27 -0500

    Update DESeq_comparison.R

--- DESeq/DESeq_comparison.R
+++ DESeq/DESeq_comparison.R
@@ -1,45 +1,46 @@
+library(dplyr)
+library(ggplot2)
+library(DESeq2)
+library(edgeR)
+library(reshape)
+
 ####################################################################
 ########### DESeq2 analysis ###############
-
-read.value %>% head()
+## read.value : raw count matrix
+## check the duplication 
 duplicated(read.value$peak) %>% table()
 rownames(read.value) <- read.value$peak
-saveRDS(read.value, 'rds/cutandrun/readvalue.2021.10.28.rename.rds')
-read.value %>% colnames()
+## file location:  'rds/cutandrun/readvalue.2021.10.28.rename.rds')
 
-
-
+## compare two groups : Tcm and Tem
 count.mtx <- read.value[,grep('tcm|tem', colnames(read.value))]
-count.mtx %>% head()
-group1 <- 'tcm'
-group2 <- 'tem'
+group1 <- 'tcm' ## control
+group2 <- 'tem' ## test
 
-
-### information sheet
+### generate the information sheet
 info <- data.frame(matrix(nrow = length(colnames(count.mtx)), ncol = 3))
 colnames(info) <- c('sample', 'cell_type','mutation')
 info$sample <- substr(colnames(count.mtx),6,9)
 info$cell_type <- substr(colnames(count.mtx),6,8)
 info$mutation <- 'no'
-info
 info$cell_type <- factor(info$cell_type, levels = c(group1,group2))
 info$mutation <- factor(info$mutation)
-str(info)
 info
 
+### generate the DESeq object
 dds <- DESeqDataSetFromMatrix(count.mtx, info, ~cell_type)
 dds <- DESeq(dds)
 
-### vst for PCA ### 
+### vst for PCA : this is a quick PCA, for the normalized data_PCA, use prcomp (in other code) ### 
 vsd <- vst(dds,blind=TRUE)
 plotPCA(vsd, intgroup="cell_type") # to remove the dependence of the variance on the mean
 pcaplot <- plotPCA(vsd, intgroup="sample", return=T)  #using the DESEQ2 plotPCA fxn we can
+
 ### drawing plot
 theme <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                panel.background = element_blank(), 
                axis.line = element_line(colour = "black"))
-ggplot(pcaplot, aes(PC1,PC2, shape=sample)) + geom_point(size=3, alpha=0.8) + 
-  theme
+ggplot(pcaplot, aes(PC1,PC2, shape=sample)) + geom_point(size=3, alpha=0.8) + theme
 
 ### log2FC quick look ###
 res <- results(dds)
@@ -50,6 +51,7 @@ saveRDS(dds, paste0(set2, '.rds'))
 
 hist(res$log2FoldChange, breaks = 100)
 hist(res$padj, breaks = 100)
+## quick look for the volcano plot
 EnhancedVolcano::EnhancedVolcano(res,
                                  lab = rownames(res),
                                  pCutoff = 0.05, FCcutoff = 1,
@@ -58,8 +60,8 @@ EnhancedVolcano::EnhancedVolcano(res,
                                  pointSize = 1.0,
                                  labSize = 4.0)
 
+## calculate the number of peaks in each condition
 res <- results(dds)
-res %>% dim()
 res <- data.frame(res)
 res %>% filter(padj <= 0.05) %>% dim()
 res %>% filter(padj <= 0.05) %>% filter(log2FoldChange >= 2 | log2FoldChange <= -2) %>% dim()
