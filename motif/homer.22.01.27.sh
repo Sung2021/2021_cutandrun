a8cc59c362eb62b52f25a43204abc55c9f823572
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-01-27 16:18:13 -0500

    Add files via upload

---
+++ motif/homer.22.01.27.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+perl /Users/sung/opt/miniconda3/share/homer-4.10-0/bin/findMotifsGenome.pl red.peak.2022.01.27.bed mm10 homer/red/
+perl /Users/sung/opt/miniconda3/share/homer-4.10-0/bin/findMotifsGenome.pl green.peak.2022.01.27.bed mm10 homer/green/
