aea16e28aa806c8c9bb41723b024d180eb3b05d9
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-03-07 12:29:23 -0500

    Create bash.sh

---
+++ sh/Tbet21/bash.sh
@@ -0,0 +1,5 @@
+### combine two fastqs to one file
+(base) mhsadmins-Mac-Pro:Cnr_Tbet_eff sung$ cat 19092FL-35-01-21_S21_R1_001.fastq.gz 19092FL-42-01-13_S469_L005_R1_001.fastq.gz > Tbet_cnr_rep1.R1.fastq.gz
+(base) mhsadmins-Mac-Pro:Cnr_Tbet_eff sung$ cat 19092FL-35-01-21_S21_R2_001.fastq.gz 19092FL-42-01-13_S469_L005_R2_001.fastq.gz > Tbet_cnr_rep1.R2.fastq.gz
+(base) mhsadmins-Mac-Pro:Cnr_Tbet_eff sung$ cat 19092FL-35-01-22_S22_R1_001.fastq.gz 19092FL-42-01-14_S470_L005_R1_001.fastq.gz > Tbet_cnr_rep2.R1.fastq.gz
+(base) mhsadmins-Mac-Pro:Cnr_Tbet_eff sung$ cat 19092FL-35-01-22_S22_R2_001.fastq.gz 19092FL-42-01-14_S470_L005_R2_001.fastq.gz > Tbet_cnr_rep2.R2.fastq.gz
