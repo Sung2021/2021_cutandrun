edba62b87d7415bb1709a8c510c286eedcfc4db6
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-05-02 10:52:49 -0400

    Add files via upload

---
+++ motif/homer.2022.04.29.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## tcm prepotent sites tem prepotent as background
+# perl /Users/sung/opt/miniconda3/share/homer-4.10-0/bin/findMotifsGenome.pl tcm.pre.bed mm10 condition_A -bg tem.pre.bed
+
+## tem prepotent sites tcm prepotent as background
+# perl /Users/sung/opt/miniconda3/share/homer-4.10-0/bin/findMotifsGenome.pl tem.pre.bed mm10 condition_B -bg tcm.pre.bed
+
+## change name 
+## condition A to group 6
+## condition B to group 5
